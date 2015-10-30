d5f827a84dd739b0ca288a0ce16a1eeb3565f911
xyz <xyz@Konrad-Komputer>
2015-10-30 21:52:15 +0100

    Dodane funkcje sprawdzajace wygrana gracza

--- TicTacToe/src/Board.java
+++ TicTacToe/src/Board.java
@@ -2,14 +2,14 @@
 public class Board {
 	private int fieldsToWin;
 	private char[][] gameBoard;
+	public int size;
 
 	public boolean setField(Player player, Position position) {
 		if (gameBoard[position.x][position.y] == 0) {
-			if(player.getId() == 1){
-			gameBoard[position.x][position.y] = 'X';
-			return true;
-			}
-			else{
+			if (player.getId() == 1) {
+				gameBoard[position.x][position.y] = 'X';
+				return true;
+			} else {
 				gameBoard[position.x][position.y] = 'O';
 				return true;
 			}
@@ -31,19 +31,90 @@ public class Board {
 	}
 
 	public boolean isPlayerWin(Player player) {
-		
-		
-		return true;
+
+		if (checkHorizontalWin(player)) {
+			return true;
+		}
+
+		if (checkVerticalWin(player)) {
+			return true;
+		}
+
+		if (checkDiagonalWin(player)) {
+			return true;
+		}
+		return false;
+
+	}
+
+	public boolean checkHorizontalWin(Player player)
+
+	{
+
+		for (int row = 0; row < size; row++) {
+			int total = 0;
+
+			for (int col = 0; col < size; col++) {
+				total += gameBoard[row][col];
+			}
+
+			if (total % player.getId() == 0) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	public boolean checkVerticalWin(Player player) {
+		for (int col = 0; col < size; col++) {
+			int total = 0;
+
+			for (int row = 0; row < size; row++) {
+				total += gameBoard[row][col];
+			}
+
+			if (total % player.getId() == 0) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	public boolean checkDiagonalWin(Player player) {
+		int total = 0;
+
+		for (int lr = 0; lr < size; lr++) {
+			total += gameBoard[lr][lr];
+		}
+
+		if (total % player.getId() == 0) {
+			return true;
+		}
+
+		total = 0;
+
+		for (int rl = 0; rl < size; rl++) {
+			total += gameBoard[rl][size - rl - 1];
+
+		}
+
+		if (total % player.getId() == 0) {
+			return true;
+		}
+		return false;
 	}
 
 	public Board(GameParams gp) {
 		this.fieldsToWin = gp.fieldsToWin;
 		this.gameBoard = new char[gp.boardSize][gp.boardSize];
+		this.size = gp.boardSize;
 	}
 
 	public void printBoard() {
-		for (int i = 0; i < gameBoard[i].length; i++) {
-			for (int j = 0; j < gameBoard[j].length; j++) {
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
 				System.out.println(gameBoard[i][j]);
 			}
 		}
