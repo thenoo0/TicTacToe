2295b3dccc8cf60d969475986eb09b029b7c8b4a
xyz <xyz@Konrad-Komputer>
2015-10-31 01:18:51 +0100

    Zarys main

--- TicTacToe/src/Main.java
+++ TicTacToe/src/Main.java
@@ -38,7 +38,7 @@ public class Main {
 		//game.startGame();
 		//activePlayer = p1 ????
 		//
-		////while(makeMoveAndCheckWin(p1)){ ///  changePlayers }
+		////while(makeMoveAndCheckWin(activePlayer)){ ///  changePlayers }
 		System.out.println("Koniec gry");
 		///koniec petli(wyjscie z petli koniec programu)
 	}
