df31328c7712046647251e0775dd188543f4ac48
xyz <xyz@Konrad-Komputer>
2015-10-31 01:16:05 +0100

    Zarys Main(dodanie komentarzy)

--- TicTacToe/src/Main.java
+++ TicTacToe/src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 //	public static void Menu(){
 //		System.out.println("Gra kolko i krzyzyk");
 //		System.out.println("blablablalbalba");	
+		////np. 1 - rozpocznij gre, 2 - wyjdz z gry(z programu)
 //	}
 	public static GameParams getGameParams(){
 		System.out.println("Podaj parametry gry:");
@@ -12,7 +13,7 @@ public class Main {
 		Scanner in = new Scanner(System.in);
 		int gameType = in.nextInt();
 		boolean isComputer;
-		if(gameType==1){
+		if(gameType==2){
 		isComputer = true;
 		}
 		else{
@@ -30,10 +31,15 @@ public class Main {
 		///while(1 - rozpocznij gre, 2 - wyjscie z programu
 		//Menu();
 		GameParams gameParams = getGameParams();
+		//Player activePlayer;
 		Player p1 = new Player(1, false);
 		Player p2 = new Player(2, gameParams.isComputer);
-		
-		System.out.println();
+		//Game game = new Game(activePlayer, p1, p2, gameParams);
+		//game.startGame();
+		//activePlayer = p1 ????
+		//
+		////while(makeMoveAndCheckWin(p1)){ ///  changePlayers }
+		System.out.println("Koniec gry");
 		///koniec petli(wyjscie z petli koniec programu)
 	}
 }
